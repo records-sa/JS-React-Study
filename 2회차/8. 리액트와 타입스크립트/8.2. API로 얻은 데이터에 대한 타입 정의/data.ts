043b33e59b4ef3dbde302f67df61ce94c2ca7046
Seoa Kim <seoa521@gmail.com>
2024-03-18 14:12:19 +0900

    [2회차] chapter 8. 스터디 완료

---
+++ 2회차/8. 리액트와 타입스크립트/8.2. API로 얻은 데이터에 대한 타입 정의/data.ts
@@ -0,0 +1,25 @@
+// 엔드포인트
+//example.com/users
+
+// 데이터 결과
+https: [
+  {
+    id: 1,
+    name: '누시다',
+    age: 24,
+    personalColor: 'blue',
+  },
+  {
+    id: 2,
+    name: '사키오카',
+    age: 28,
+    personalColor: 'pink',
+  },
+  {
+    id: 3,
+    name: '고토',
+    age: 23,
+    personalColor: 'green',
+    hobbies: ['game', 'soccer'],
+  },
+];
---
+++ 2회차/8. 리액트와 타입스크립트/8.2. API로 얻은 데이터에 대한 타입 정의/타입스크립트화/src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { axios } from 'axios';
+import { ListItem } from './components/ListItem';
+import { User } from './types/user';
+
+export const App = () => {
+  // 얻은 사용자 정보
+  const [users, setUsers] = useState<User[]>([]);
+
+  // 화면에 표시될 때 사용자 정보 얻기
+  useEffect(() => {
+    axios.get<User[]>('https://example.com/users').then((res) => {
+      setUsers(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      {users.map((user) => (
+        <ListItem
+          id={user.id}
+          name={user.name}
+          age={user.age}
+          personalColor={user.personalColor}
+          hobbies={user.hobbies}
+        />
+      ))}
+    </div>
+  );
+};
